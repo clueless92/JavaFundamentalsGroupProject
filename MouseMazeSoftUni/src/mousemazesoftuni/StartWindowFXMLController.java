59b20780365fc1aa3d6eae7ffbe66989a5850fc1
clueless92 <tilchev92@gmail.com>
2015-10-31 19:27:46 +0200

    Make LevelOne Start Button in start window

--- MouseMazeSoftUni/src/mousemazesoftuni/StartWindowFXMLController.java
+++ MouseMazeSoftUni/src/mousemazesoftuni/StartWindowFXMLController.java
@@ -5,15 +5,23 @@
  */
 package mousemazesoftuni;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 /**
  *
@@ -21,25 +29,25 @@ import javafx.scene.input.MouseEvent;
  */
 public class StartWindowFXMLController implements Initializable {
     
+    private static Stage stage;
     @FXML
-    private Label label;
-    @FXML
-    private Button button;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    private Button startLevelOneButton;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 
     @FXML
-    private void onDragDetected(MouseEvent event) {
-        Platform.exit();
+    private void onStartLevelOne(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("LevelOneFXML.fxml"));
+        Scene scene = new Scene(root);
+        stage = new Stage();
+        stage.setScene(scene);
+        //stage.setOnHidden(this);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setResizable(false);
+        stage.show();
     }
     
 }
